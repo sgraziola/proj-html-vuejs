d16a07f45d1810c22460f3504a5f81461cbc0269
sgraziola <simo.fdl@gmail.com>
2022-11-21 12:18:02 +0100

    MasterStudy: Initial commit, create scaffolding

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<template>
+  <header id="site_header"></header>
+  <!-- /#site_header -->
+  <main id="site_main"></main>
+  <!-- site_main -->
+  <footer id="site_footer"></footer>
+  <!-- /#site_footer -->
+</template>
+
+<style scoped>
+
+</style>
