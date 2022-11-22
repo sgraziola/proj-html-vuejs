9c551bd40feaf1c22ad41364769ab0ad08100ebd
sgraziola <simo.fdl@gmail.com>
2022-11-23 00:11:42 +0100

    MasterStudy: complete CallToAction section

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -5,34 +5,49 @@ export default {
 </script>
 
 <template>
-    <section class="callToAction">
-
-        <div class="card col-6">
-            <img src="../assets/img/image_box_1-221x231.png" class="card-img-left" alt="">
+    <section class="callToAction main_container d-flex justify-content-center py-5 my-2">
+        <div class="card flex-row rounded-0 me-2 p-5">
+            <img src="../assets/img/image_box_1-221x231.png" alt="">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h3 class="card-title mb-4">Become an Instructor</h3>
+                <p class="card-text col-9">Teach what you love. Masterstudy gives you the tools to create a course.</p>
+                <a href="#"><button class="btn_sign">START TEACHING</button></a>
             </div>
         </div>
-        <div class="card col-6">
-            <img src="../assets/img/image_box_2-234x231.png" class="card-img-top" alt="">
+        <div class="card flex-row rounded-0 ms-2 p-5">
+            <img src="../assets/img/image_box_2-234x231.png" alt="">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h3 class="card-title mb-4">Access for Business</h3>
+                <p class="card-text col-9">Get unlimited access to 2,500 of top courses for your team.</p>
+                <a href="#"><button class="btn_sign">DOING BUSINESS</button></a>
             </div>
         </div>
     </section>
 </template>
 
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
 .callToAction {
-    .col-6 {
-        img {
-            width: 50px;
+    .card {
+        width: 50%;
+
+        .card-body {
+            margin-left: 2rem;
+
+            h3 {
+                font-size: 34px;
+            }
+
+            p {
+                font-size: 17px;
+                color: $ms_txt_header;
+                margin-bottom: 2rem;
+            }
+
+            button:hover {
+                background-color: $ms_hover;
+            }
         }
     }
 }
